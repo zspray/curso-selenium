2731d5eedab32d34c1eb15b9102ba74da3ff2aea
Spray <marcosbandeira@live.com>
2025-12-27 23:20:54 -0300

    Update TesteCampoTreinamento.java

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Assert;
 
 public class TesteCampoTreinamento {
 	
@@ -12,8 +14,21 @@ public class TesteCampoTreinamento {
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().setSize(new Dimension(800, 600));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
-//		driver.quit();
-		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de Escritaaaa");
+		Assert.assertEquals("Teste de Escritaaaa", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		driver.close();
 	}
+	
+	
+	@Test
+	public void deveInteragirComTextArea() {
+		System.setProperty("webdriver.gecko.driver", "C:/Users/marco/Downloads/drivers/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(800, 600));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste de Escritaaaa");
+		Assert.assertEquals("Teste de Escritaaaa", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		driver.close();
+	}
+	
 }
